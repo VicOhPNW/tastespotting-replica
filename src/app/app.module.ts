9d63e98df123bfe890766759a33861c1ffbe5b37
Victoria Oh <ohvictori@gmail.com>
2018-10-26 10:09:56 -0700

    Create body, footer, and search components. Confirm each component works (is visible on root comoponent.)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
