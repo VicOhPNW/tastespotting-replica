ac127ba2510f8cf48276678e2362c753ea3c806e
Victoria Oh <ohvictori@gmail.com>
2018-10-26 09:47:05 -0700

    Create header and nav-bar components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
