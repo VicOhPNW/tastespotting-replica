76aa7802f030d18a0db108ff6f989d67ca18b727
Victoria Oh <ohvictori@gmail.com>
2018-10-26 14:03:51 -0700

    Delete/remove references to body component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { SearchComponent } from './search/search.component';
 import { AdvertComponent } from './advert/advert.component';
@@ -16,7 +15,6 @@ import { AdvertComponent } from './advert/advert.component';
     AppComponent,
     NavBarComponent,
     HeaderComponent,
-    BodyComponent,
     FooterComponent,
     SearchComponent,
     AdvertComponent
