39f30b1a77005c0b9284ef6f9dc5f48f136f9e20
Victoria Oh <ohvictori@gmail.com>
2018-11-02 11:01:15 -0700

    Figure out nested routing. But have not used - yet.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,14 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PanelComponent } from './panel/panel.component';
+import { AdvertComponent } from './advert/advert.component';
 
 const appRoutes: Routes = [
     {
       path: '',
       component: PanelComponent
     },
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
