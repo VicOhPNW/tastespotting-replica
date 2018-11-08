3f3f8485a12c6abd2c0d3ef45e5b96734ee5f859
Victoria Oh <ohvictori@gmail.com>
2018-11-08 12:25:09 -0800

    Remove api-keys from being ignored via .gitignore. Reason: project evaluation need.

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyCPwtRY0klb7AsSFRYsVNICdPpeXaWEamE",
+    authDomain: "tastespotting-replica.firebaseapp.com",
+    databaseURL: "https://tastespotting-replica.firebaseio.com",
+    storageBucket: "tastespotting-replica.appspot.com",
+    messagingSenderId: "813861377066"
+  };
